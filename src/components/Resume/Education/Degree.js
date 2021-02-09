561e1d757781512190f6285a7991183bb6eb27b0
ssusanto2706 <samantha.susanto@gmail.com>
2021-02-09 15:49:44 -0800

    multi degree support

--- src/components/Resume/Education/Degree.js
+++ src/components/Resume/Education/Degree.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Degree = ({ data }) => (
   <article className="degree-container">
     <header>
-      <h4 className="degree">{data.degree}</h4>
+      {data.degree.map((d) => (<h4 className="degree">{d}</h4>))}
       <p className="school"><a href={data.link}>{data.school}</a>, {data.year}</p>
     </header>
   </article>
