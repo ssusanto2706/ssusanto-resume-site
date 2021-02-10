0985c5fb79fb259b981543529a25076f8d4edfed
ssusanto2706 <samantha.susanto@gmail.com>
2021-02-10 15:11:32 -0800

    personalize home page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,24 +5,21 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Samantha J Susanto's personal website. New York based Stanford ICME graduate, "
-    + 'co-founder and CTO of Arthena, and YC Alumni.'}
+    description={"Samantha J Susanto's personal website."}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <h2 data-testid="heading"><Link to="/">About Me</Link></h2>
           <p>
             A beautiful, responsive, statically-generated,
             react application written with modern Javascript.
           </p>
         </div>
       </header>
-      <p> Welcome to my personal website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
+      <p> Welcome to my personal website. You can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
+        or <Link to="/projects">projects</Link>.
       </p>
     </article>
   </Main>
